6b4e48fb13a6708088e4c4fc8cb486756ebc7b23
Karine Ferreira Mendes <karineferreiramendes@gmail.com>
2025-07-17 16:17:30 -0300

    header and characters page updated

---
+++ js/players.js
@@ -0,0 +1,462 @@
+const playerData = {
+    karasuno: {
+        daichi: {
+          name: "Sawamura Daichi",
+          grade: "3rd year",
+          position: "Wing Spiker",
+          height: "1.76m",
+          birthday: "December 31",
+          number: 1,
+          img: "assets/karasuno/daichi.png",
+          thumbnail: "assets/karasuno/daichi-thumb.png"
+        },
+        sugawara: {
+          name: "Sugawara Koshi",
+          grade: "3rd year",
+          position: "Setter",
+          height: "1.74m",
+          birthday: "June 13",
+          number: 2,
+          img: "assets/karasuno/sugawara.png",
+          thumbnail: "assets/karasuno/sugawara-thumb.png"
+        },
+        asahi: {
+          name: "Azumane Asahi",
+          grade: "3rd year",
+          position: "Wing Spiker",
+          height: "1.86m",
+          birthday: "January 1",
+          number: 3,
+          img: "assets/karasuno/asahi.png",
+          thumbnail: "assets/karasuno/asahi-thumb.png"
+        },
+        nishinoya: {
+          name: "Nishinoya Yu",
+          grade: "2nd year",
+          position: "Libero",
+          height: "1.60m",
+          birthday: "October 10",
+          number: 4,
+          img: "assets/karasuno/nishinoya.png",
+          thumbnail: "assets/karasuno/nishinoya-thumb.png"
+        },
+        tanaka: {
+          name: "Tanaka Ryunosuke",
+          grade: "2nd year",
+          position: "Wing Spiker",
+          height: "1.78m",
+          birthday: "March 3",
+          number: 5,
+          img: "assets/karasuno/tanaka.png",
+          thumbnail: "assets/karasuno/tanaka-thumb.png"
+        },
+        ennoshita: {
+          name: "Ennoshita Chikara",
+          grade: "2nd year",
+          position: "Wing Spiker",
+          height: "1.76m",
+          birthday: "December 26",
+          number: 6,
+          img: "assets/karasuno/ennoshita.png",
+          thumbnail: "assets/karasuno/ennoshita-thumb.png"
+        },
+        kinoshita: {
+          name: "Kinoshita Hisashi",
+          grade: "2nd year",
+          position: "Wing Spiker",
+          height: "1.75m",
+          birthday: "February 15",
+          number: 7,
+          img: "assets/karasuno/kinoshita.png",
+          thumbnail: "assets/karasuno/kinoshita-thumb.png"
+        },
+        narita: {
+          name: "Narita Kazuhito",
+          grade: "2nd year",
+          position: "Middle Blocker",
+          height: "1.80m",
+          birthday: "August 17",
+          number: 8,
+          img: "assets/karasuno/narita.png",
+          thumbnail: "assets/karasuno/narita-thumb.png"
+        },
+        kageyama: {
+          name: "Kageyama Tobio",
+          grade: "1st year",
+          position: "Setter",
+          height: "1.81m",
+          birthday: "December 22",
+          number: 9,
+          img: "assets/karasuno/kageyama.png",
+          thumbnail: "assets/karasuno/kageyama-thumb.png"
+        },
+        hinata: {
+          name: "Hinata Shoyo",
+          grade: "1st year",
+          position: "Middle Blocker",
+          height: "1.64m",
+          birthday: "June 21",
+          number: 10,
+          img: "assets/karasuno/hinata.png",
+          thumbnail: "assets/karasuno/hinata-thumb.png"
+        },
+        tsukishima: {
+          name: "Tsukishima Kei",
+          grade: "1st year",
+          position: "Middle Blocker",
+          height: "1.90m",
+          birthday: "September 27",
+          number: 11,
+          img: "assets/karasuno/tsukishima.png",
+          thumbnail: "assets/karasuno/tsukishima-thumb.png"
+        },
+        yamaguchi: {
+          name: "Yamaguchi Tadashi",
+          grade: "1st year",
+          position: "Middle Blocker",
+          height: "1.80m",
+          birthday: "November 10",
+          number: 12,
+          img: "assets/karasuno/yamaguchi.png",
+          thumbnail: "assets/karasuno/yamaguchi-thumb.png"
+        }
+      },
+    seijou: {
+        oikawa: {
+          name: "Oikawa Toru",
+          grade: "3rd year",
+          position: "Setter",
+          height: "1.84m",
+          birthday: "July 20",
+          number: 1,
+          img: "assets/seijou/oikawa.png",
+          thumbnail: "assets/seijou/oikawa-thumb.png"
+        },
+        issei: {
+            name: "Matsukawa Issei",
+            grade: "3rd year",
+            position: "Middle Blocker",
+            height: "1.87m",
+            birthday: "March 1",
+            number: 2,
+            img: "assets/seijou/issei.png",
+            thumbnail: "assets/seijou/issei-thumb.png"
+          },
+        takahiro: {
+            name: "Hanamaki Takahiro",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.84m",
+            birthday: "January 27",
+            number: 3,
+            img: "assets/seijou/takahiro.png",
+            thumbnail: "assets/seijou/takahiro-thumb.png"
+          },
+        iwaizumi: {
+            name: "Iwaizumi Hajime",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.79m",
+            birthday: "June 10",
+            number: 4,
+            img: "assets/seijou/iwaizumi.png",
+            thumbnail: "assets/seijou/iwaizumi-thumb.png"
+          },
+        yahaba: {
+            name: "Yahaba Shigeru",
+            grade: "2nd year",
+            position: "Setter",
+            height: "1.81m",
+            birthday: "March 1",
+            number: 5,
+            img: "assets/seijou/yahaba.png",
+            thumbnail: "assets/seijou/yahaba-thumb.png"
+          },
+        watari: {
+            name: "Watari Shinji",
+            grade: "2nd year",
+            position: "Libero",
+            height: "1.71m",
+            birthday: "April 3",
+            number: 7,
+            img: "assets/seijou/watari.png",
+            thumbnail: "assets/seijou/watari-thumb.png"
+          },
+        kindaichi: {
+            name: "Kindaichi Yutaro",
+            grade: "1st year",
+            position: "Middle Blocker",
+            height: "1.89m",
+            birthday: "June 6",
+            number: 12,
+            img: "assets/seijou/kindaichi.png",
+            thumbnail: "assets/seijou/kindaichi-thumb.png"
+          },
+        kunimi: {
+            name: "Kunimi Akira",
+            grade: "1st year",
+            position: "Wing Spiker",
+            height: "1.82m",
+            birthday: "March 25",
+            number: 13,
+            img: "assets/seijou/kunimi.png",
+            thumbnail: "assets/seijou/kunimi-thumb.png"
+          },
+        kentaro: {
+            name: "Kyotani Kentaro",
+            grade: "2nd year",
+            position: "Wing Spiker",
+            height: "1.78m",
+            birthday: "December 7",
+            number: 16,
+            img: "assets/seijou/kentaro.png",
+            thumbnail: "assets/seijou/kentaro-thumb.png"
+          }
+    },
+    inarizaki: {
+        kita: {
+          name: "Kita Shinsuke",
+          grade: "3rd year",
+          position: "Wing Spiker",
+          height: "1.75m",
+          birthday: "July 5",
+          number: 1,
+          img: "assets/inarizaki/kita.png",
+          thumbnail: "assets/inarizaki/kita-thumb.png"
+        },
+        omimi: {
+            name: "Omimi Ren",
+            grade: "3rd year",
+            position: "Middle Blocker",
+            height: "1.91m",
+            birthday: "December 31",
+            number: 2,
+            img: "assets/inarizaki/omimi.png",
+            thumbnail: "assets/inarizaki/omimi-thumb.png"
+          },
+        aran: {
+            name: "Ojiro Aran",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.84m",
+            birthday: "April 4",
+            number: 4,
+            img: "assets/inarizaki/aran.png",
+            thumbnail: "assets/inarizaki/aran-thumb.png"
+          },
+        ginjima: {
+            name: "Ginjima Hitoshi",
+            grade: "2nd year",
+            position: "Wing Spiker",
+            height: "1.80m",
+            birthday: "August 21",
+            number: 5,
+            img: "assets/inarizaki/ginjima.png",
+            thumbnail: "assets/inarizaki/ginjima-thumb.png"
+          },
+        atsumu: {
+            name: "Miya Atsumu",
+            grade: "2nd year",
+            position: "Setter",
+            height: "1.83m",
+            birthday: "October 5",
+            number: 7,
+            img: "assets/inarizaki/atsumu.png",
+            thumbnail: "assets/inarizaki/atsumu-thumb.png"
+          },
+        suna: {
+            name: "Suna Rintaro",
+            grade: "2nd year",
+            position: "Middle Blocker",
+            height: "1.85m",
+            birthday: "January 25",
+            number: 10,
+            img: "assets/inarizaki/suna.png",
+            thumbnail: "assets/inarizaki/suna-thumb.png"
+          },
+        osamu: {
+            name: "Miya Osamu",
+            grade: "2nd year",
+            position: "Wing Spiker",
+            height: "1.83m",
+            birthday: "October 5",
+            number: 11,
+            img: "assets/inarizaki/osamu.png",
+            thumbnail: "assets/inarizaki/osamu-thumb.png"
+          },
+        akagi: {
+            name: "Akagi Michinari",
+            grade: "3rd year",
+            position: "Libero",
+            height: "1.74m",
+            birthday: "April 12",
+            number: 15,
+            img: "assets/inarizaki/akagi.png",
+            thumbnail: "assets/inarizaki/akagi-thumb.png"
+          }
+    },
+    nekoma: {
+        kuroo: {
+            name: "Kuroo Tetsuro",
+            grade: "3rd year",
+            position: "Middle Blocker",
+            height: "1.87m",
+            birthday: "November 17",
+            number: 1,
+            img: "assets/nekoma/kuroo.png",
+            thumbnail: "assets/nekoma/kuroo-thumb.png"
+          },
+          kai: {
+            name: "Kai Nobuyuki",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.76m",
+            birthday: "April 8",
+            number: 2,
+            img: "assets/nekoma/kai.png",
+            thumbnail: "assets/nekoma/kai-thumb.png"
+          },
+          yaku: {
+            name: "Yaku Morisuke",
+            grade: "3rd year",
+            position: "Libero",
+            height: "1.65m",
+            birthday: "August 8",
+            number: 3,
+            img: "assets/nekoma/yaku.png",
+            thumbnail: "assets/nekoma/yaku-thumb.png"
+          },
+          yamamoto: {
+            name: "Yamamoto Taketora",
+            grade: "2nd year",
+            position: "Wing Spiker",
+            height: "1.76m",
+            birthday: "February 22",
+            number: 4,
+            img: "assets/nekoma/yamamoto.png",
+            thumbnail: "assets/nekoma/yamamoto-thumb.png"
+          },
+          kenma: {
+            name: "Kozume Kenma",
+            grade: "2nd year",
+            position: "Setter",
+            height: "1.69m",
+            birthday: "October 16",
+            number: 5,
+            img: "assets/nekoma/kenma.png",
+            thumbnail: "assets/nekoma/kenma-thumb.png"
+          },
+          fukunaga: {
+            name: "Fukunaga Shohei",
+            grade: "2nd year",
+            position: "Wing Spiker",
+            height: "1.78m",
+            birthday: "September 29",
+            number: 6,
+            img: "assets/nekoma/fukunaga.png",
+            thumbnail: "assets/nekoma/fukunaga-thumb.png"
+          },
+          inuoka: {
+            name: "Inuoka So",
+            grade: "1st year",
+            position: "Middle Blocker",
+            height: "1.85m",
+            birthday: "November 1",
+            number: 7,
+            img: "assets/nekoma/inuoka.png",
+            thumbnail: "assets/nekoma/inuoka-thumb.png"
+          },
+          lev: {
+            name: "Haiba Lev",
+            grade: "1st year",
+            position: "Middle Blocker",
+            height: "1.94m",
+            birthday: "October 30",
+            number: 11,
+            img: "assets/nekoma/lev.png",
+            thumbnail: "assets/nekoma/lev-thumb.png"
+          }
+      },
+    shiratorizawa : {
+        ushijima: {
+            name: "Ushijima Wakatoshi",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.89m",
+            birthday: "August 13",
+            number: 1,
+            img: "assets/shiratorizawa/ushijima.png",
+            thumbnail: "assets/shiratorizawa/ushijima-thumb.png"
+          },
+          semi: {
+            name: "Semi Eita",
+            grade: "3rd year",
+            position: "Setter",
+            height: "1.83m",
+            birthday: "October 5",
+            number: 3,
+            img: "assets/shiratorizawa/semi.png",
+            thumbnail: "assets/shiratorizawa/semi-thumb.png"
+          },
+          ohira: {
+            name: "Ohira Reon",
+            grade: "3rd year",
+            position: "Wing Spiker",
+            height: "1.82m",
+            birthday: "October 30",
+            number: 4,
+            img: "assets/shiratorizawa/ohira.png",
+            thumbnail: "assets/shiratorizawa/ohira-thumb.png"
+          },
+          tendo: {
+            name: "Tendo Satori",
+            grade: "3rd year",
+            position: "Middle Blocker",
+            height: "1.87m",
+            birthday: "May 20",
+            number: 5,
+            img: "assets/shiratorizawa/tendo.png",
+            thumbnail: "assets/shiratorizawa/tendo-thumb.png"
+          },
+          goshiki: {
+            name: "Goshiki Tsutomu",
+            grade: "1st year",
+            position: "Wing Spiker",
+            height: "1.81m",
+            birthday: "August 22",
+            number: 8,
+            img: "assets/shiratorizawa/goshiki.png",
+            thumbnail: "assets/shiratorizawa/goshiki-thumb.png"
+          },
+          kenjiro: {
+            name: "Shirabu Kenjiro",
+            grade: "2nd year",
+            position: "Setter",
+            height: "1.74m",
+            birthday: "May 4",
+            number: 10,
+            img: "assets/shiratorizawa/kenjiro.png",
+            thumbnail: "assets/shiratorizawa/kenjiro-thumb.png"
+          },
+          taichi: {
+            name: "Kawanishi Taichi",
+            grade: "2nd year",
+            position: "Middle Blocker",
+            height: "1.88m",
+            birthday: "April 15",
+            number: 12,
+            img: "assets/shiratorizawa/taichi.png",
+            thumbnail: "assets/shiratorizawa/taichi-thumb.png"
+          },
+          yamagata: {
+            name: "Yamagata Hayato",
+            grade: "3rd year",
+            position: "Libero",
+            height: "1.74m",
+            birthday: "February 14",
+            number: 14,
+            img: "assets/shiratorizawa/yamagata.png",
+            thumbnail: "assets/shiratorizawa/yamagata-thumb.png"
+          }
+      }
+  }
---
+++ js/script.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("components/header.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+
+      // show mobile menu
+      const showMenu = (toggleId, navId) => {
+        const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+
+        toggle.addEventListener("click", () => {
+          nav.classList.toggle("show-menu");
+          toggle.classList.toggle("show-icon");
+        });
+      };
+      showMenu("nav-toggle", "nav-menu");
+
+      // Dropdowns mobile
+      const dropdownItems = document.querySelectorAll(".dropdown__item");
+
+      dropdownItems.forEach((item) => {
+        const link = item.querySelector(".nav__link");
+
+        link.addEventListener("click", () => {
+          dropdownItems.forEach((i) => {
+            if (i !== item) i.classList.remove("dropdown-open");
+          });
+
+          item.classList.toggle("dropdown-open");
+        });
+      });
+    });
+});
+
+// players section
+document.addEventListener("DOMContentLoaded", () => {
+  const school = document.body.dataset.school;
+  const players = playerData[school];
+
+  if (!players) {
+    console.warn("School not found.");
+    return;
+  }
+
+  const playerKeys = Object.keys(players);
+  let currentIndex = 0;
+
+  const indicatorContainer = document.getElementById("player-indicators");
+  const playerImg = document.getElementById("player-img");
+  const playerInfo = document.getElementById("player-info");
+  const playerNumber = document.getElementById("player-number");
+
+  const updatePlayer = (index) => {
+    const key = playerKeys[index];
+    const player = players[key];
+
+    playerImg.src = player.img;
+    playerImg.alt = player.name;
+
+    playerInfo.innerHTML = `
+      <h2>${player.name}</h2>
+      <p><strong>Grade:</strong> ${player.grade}</p>
+      <p><strong>Position:</strong> ${player.position}</p>
+      <p><strong>Height:</strong> ${player.height}</p>
+      <p><strong>Birthday:</strong> ${player.birthday}</p>
+    `;
+
+    playerNumber.textContent = player.number.toString().padStart(2, "0");
+
+    document.querySelectorAll(".indicator").forEach((el, i) => {
+      el.classList.toggle("active", i === index);
+    });
+  };
+
+  const renderIndicators = () => {
+    indicatorContainer.innerHTML = playerKeys
+      .map(
+        (key, i) => `
+      <img 
+        src="${players[key].thumbnail || players[key].img}" 
+        alt="${players[key].name}" 
+        class="indicator ${i === currentIndex ? "active" : ""}" 
+        data-index="${i}"
+      />
+    `
+      )
+      .join("");
+  };
+
+  indicatorContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("indicator")) {
+      currentIndex = parseInt(e.target.dataset.index);
+      updatePlayer(currentIndex);
+      renderIndicators();
+    }
+  });
+
+  document.getElementById("prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + playerKeys.length) % playerKeys.length;
+    updatePlayer(currentIndex);
+    renderIndicators();
+  });
+
+  document.getElementById("next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % playerKeys.length;
+    updatePlayer(currentIndex);
+    renderIndicators();
+  });
+
+  // initial display
+  updatePlayer(currentIndex);
+  renderIndicators();
+});
